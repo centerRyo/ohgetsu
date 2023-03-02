105ab0e9b66e6bddc8ebf5f8663c2447466a8346
centerRyo <abc.atrn@gmail.com>
2023-03-02 20:14:08 +0900

    srcの中に入れる

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import React from 'react';
-import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
