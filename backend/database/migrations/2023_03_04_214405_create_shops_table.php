e21e4f2695ce70be529ab6e1fd705404f3893e2a
centerRyo <abc.atrn@gmail.com>
2023-03-04 21:47:15 +0900

    shopsテーブル作成

---
+++ backend/database/migrations/2023_03_04_214405_create_shops_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('店名');
+            $table->string('address')->nullable()->comment('住所');
+            $table->string('pic')->nullable()->comment('写真');
+            $table->foreignId('genre_id')->constrained('genres')->comment("ジャンルID");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
