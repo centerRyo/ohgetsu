a8ffe276953136c7e3273c6da3a81d65d0cfe98a
centerRyo <abc.atrn@gmail.com>
2023-03-04 21:43:19 +0900

    ingredients table作成

---
+++ backend/database/migrations/2023_03_04_212909_create_ingredients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('原材料名');
+            $table->enum('category', ['MAIN', 'SUB'])->comment('特定原材料か特定原材料に準ずるか');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
