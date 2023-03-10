439c3cc2f0a74dd2fc8a5d7a388c965327f28d8e
centerRyo <abc.atrn@gmail.com>
2023-03-10 16:38:55 +0900

    code generatorで型生成する

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  cache: new InMemoryCache(),
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
-}
+};
 
 export default MyApp;
