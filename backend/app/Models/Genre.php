ba8e7a254573f6b154b64ade90c9f3dd60f86e1d
centerRyo <abc.atrn@gmail.com>
2023-04-08 21:50:59 +0900

    shopとgenreのリレーション設定

--- backend/app/Models/Genre.php
+++ backend/app/Models/Genre.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function shop() {
+        return $this->belongsTo('App\Models\Shop');
+    }
 }
--- backend/app/Models/Shop.php
+++ backend/app/Models/Shop.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'address',
+        'pic',
+        'genre_id'
+    ];
+
+    public function genre() {
+        return $this->hasOne('App\Models\Genre', 'id', 'genre_id');
+    }
 }
