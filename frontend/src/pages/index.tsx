a01f11c31d3114f9eb6c1675cfc39295b64749c9
centerRyo <abc.atrn@gmail.com>
2023-04-12 20:18:16 +0900

    /register_shopへのリンク追加

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const SamplePage: NextPage = () => {
-  return <div>Top</div>;
+  return <Link href='/register_shop'>お店情報登録ページへ</Link>;
 };
 
 export default SamplePage;
