82e2befc52eb76fe873fe839d07cf9bf45d6da7c
centerRyo <abc.atrn@gmail.com>
2023-04-15 11:56:48 +0900

    graphqlのテスト追加

---
+++ backend/tests/graphql/SchemaTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\Shop;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Nuwave\Lighthouse\Testing\RefreshesSchemaCache;
+use Tests\CreatesApplication;
+use Tests\TestCase;
+
+class SchemaTest extends TestCase
+{
+	use MakesGraphQLRequests;
+	use RefreshesSchemaCache;
+
+	/** @test */
+    public function ショップ一覧クエリを返却する(): void
+    {
+		$shops = Shop::factory(2)->create();
+		$this->graphQL('
+			{
+				shops {
+					name
+					address
+					genre {
+						name
+					}
+					pic
+				}
+			}
+		')->assertJson([
+			'data' => [
+				'shops' => [
+					[
+						'name' => $shops->first()->name,
+						'address' => $shops->first()->address,
+						'genre' => [
+							'name' => $shops->first()->genre->name
+						],
+						'pic' => $shops->first()->pic
+					],
+					[
+						'name' => $shops->last()->name,
+						'address' => $shops->last()->address,
+						'genre' => [
+							'name' => $shops->last()->genre->name
+						],
+						'pic' => $shops->last()->pic
+					],
+				]
+			]
+		]);
+    }
+}
