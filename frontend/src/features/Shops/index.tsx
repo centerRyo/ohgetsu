9a991cc367aa1751ba2eaaf1d5888408bbf8162c
centerRyo <abc.atrn@gmail.com>
2023-04-16 17:52:18 +0900

    お店一覧に表示するカード作成

--- frontend/src/features/Shops/index.tsx
+++ frontend/src/features/Shops/index.tsx
@@ -1,7 +1,37 @@
+import {
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import Link from 'next/link';
 import { memo } from 'react';
+import styles from './index.module.scss';
 
 const Shops = memo(() => {
-  return <div>Shops Page</div>;
+  return (
+    <div className={styles.container}>
+      <SimpleGrid
+        spacing={2}
+        templateColumns='repeat(auto-fill, minmax(200px, 1fr))'
+      >
+        <Link href=''>
+          <a>
+            <Card>
+              <CardBody>
+                <Image src='/images/no_image.png' fit='fill' />
+                <Heading size='md'>ちゃぶとん 横浜店</Heading>
+                <span className={styles.genre}>ラーメン</span>
+                <Text mt={4}>神奈川県横浜市</Text>
+              </CardBody>
+            </Card>
+          </a>
+        </Link>
+      </SimpleGrid>
+    </div>
+  );
 });
 Shops.displayName = 'Shops';
 
