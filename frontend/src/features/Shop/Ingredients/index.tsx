250d3b8c50f26fffa518f1ce05fae86a489c46e3
centerRyo <abc.atrn@gmail.com>
2023-05-16 22:19:23 +0900

    react hook formの導入(途中まで)

--- frontend/src/features/Shop/Ingredients/index.tsx
+++ frontend/src/features/Shop/Ingredients/index.tsx
@@ -2,7 +2,6 @@ import { useShopPageIngredientsQuery } from '@/graphql/generated';
 import { useCustomOptionsWithPic } from '@/hooks/useOptions';
 import {
   Flex,
-  FormControl,
   FormLabel,
   Input,
   SimpleGrid,
@@ -11,9 +10,15 @@ import {
 } from '@chakra-ui/react';
 import Image from 'next/image';
 import { FC, memo } from 'react';
+import { Control, Controller } from 'react-hook-form';
+import { FormValues } from '../index.d';
 import styles from './index.module.scss';
 
-const Ingredients: FC = memo(() => {
+type Props = {
+  control: Control<FormValues, any>;
+};
+
+const Ingredients: FC<Props> = memo(({ control }) => {
   const { data, loading } = useShopPageIngredientsQuery();
   const ingredients = data?.ingredients || [];
 
@@ -26,57 +31,63 @@ const Ingredients: FC = memo(() => {
 
   return (
     <div className={styles.container}>
-      <form>
-        <SimpleGrid
-          spacing={4}
-          templateColumns='repeat(auto-fill, minmax(150px, 1fr))'
-        >
-          {!loading
-            ? options.map((option) => (
-                <FormControl
-                  position='relative'
-                  key={option.key}
-                  className={styles.ingredient}
-                >
-                  <Input
-                    visibility='hidden'
-                    position='absolute'
-                    type='checkbox'
-                    id={option.key}
-                    name={option.value}
-                    className={styles.checkbox}
-                  />
-                  <FormLabel htmlFor={option.key} className={styles.label}>
-                    <Flex
-                      maxW='sm'
-                      padding='25px 10px'
-                      flexDir='column'
-                      alignItems='center'
-                      gap={4}
-                    >
-                      <Image
-                        loader={() =>
-                          `${process.env.NEXT_PUBLIC_BACKEND_URL}${
+      <SimpleGrid
+        spacing={4}
+        templateColumns='repeat(auto-fill, minmax(150px, 1fr))'
+      >
+        {!loading ? (
+          <Controller
+            name='ingredients'
+            control={control}
+            render={() => (
+              <>
+                {options.map((option) => (
+                  <div key={option.key} className={styles.ingredient}>
+                    <Input
+                      visibility='hidden'
+                      position='absolute'
+                      type='checkbox'
+                      id={option.key}
+                      value={option.value}
+                      name={option.value}
+                      className={styles.checkbox}
+                    />
+                    <FormLabel htmlFor={option.key} className={styles.label}>
+                      <Flex
+                        maxW='sm'
+                        padding='25px 10px'
+                        flexDir='column'
+                        alignItems='center'
+                        gap={4}
+                      >
+                        <Image
+                          loader={() =>
+                            `${process.env.NEXT_PUBLIC_BACKEND_URL}${
+                              option?.pic ?? ''
+                            }`
+                          }
+                          unoptimized
+                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${
                             option?.pic ?? ''
-                          }`
-                        }
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${
-                          option?.pic ?? ''
-                        }`}
-                        alt={option.label}
-                        width='100%'
-                        height='100%'
-                      />
-                      <Text fontWeight='bold'>{option.label}</Text>
-                    </Flex>
-                  </FormLabel>
-                </FormControl>
-              ))
-            : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_) => (
-                <Skeleton width='165px' height='190px' key={_} />
-              ))}
-        </SimpleGrid>
-      </form>
+                          }`}
+                          alt={option.label}
+                          width='100%'
+                          height='100%'
+                        />
+                        <Text fontWeight='bold'>{option.label}</Text>
+                      </Flex>
+                    </FormLabel>
+                  </div>
+                ))}
+              </>
+            )}
+          />
+        ) : (
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_) => (
+            <Skeleton width='165px' height='190px' key={_} />
+          ))
+        )}
+      </SimpleGrid>
     </div>
   );
 });
--- frontend/src/features/Shop/index.tsx
+++ frontend/src/features/Shop/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Flex, Heading } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { FC, memo, useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { FormValues } from './index.d';
 import styles from './index.module.scss';
 import Ingredients from './Ingredients';
 
@@ -11,46 +13,51 @@ const Shop: FC<Props> = memo(({}) => {
 
   const handleBack = useCallback(() => router.push('/'), [router]);
 
+  const { handleSubmit, control } = useForm<FormValues>();
+
   return (
     <div className={styles.container}>
-      <Heading mb={8}>ちゃぶとんとんこつラーメンヨドバシ横浜店</Heading>
+      <form onSubmit={handleSubmit((values) => console.log(values))}>
+        <Heading mb={8}>ちゃぶとんとんこつラーメンヨドバシ横浜店</Heading>
 
-      <section className={styles.explanation}>
-        <div className={styles.wrapper}>
-          <p>アレルギー物質を選択してください</p>
-          <p>(複数選択可)</p>
-          <p>
-            (選択した物質を<span className={styles.unused}>使用していない</span>
-            メニューが表示されます)
-          </p>
-        </div>
-      </section>
+        <section className={styles.explanation}>
+          <div className={styles.wrapper}>
+            <p>アレルギー物質を選択してください</p>
+            <p>(複数選択可)</p>
+            <p>
+              (選択した物質を
+              <span className={styles.unused}>使用していない</span>
+              メニューが表示されます)
+            </p>
+          </div>
+        </section>
 
-      <Ingredients />
+        <Ingredients control={control} />
 
-      <Flex justifyContent='center'>
-        <Button
-          size='lg'
-          type='submit'
-          colorScheme='green'
-          width='100%'
-          maxWidth='30rem'
-        >
-          検索
-        </Button>
-      </Flex>
-      <Flex justifyContent='center' mt={8}>
-        <Button
-          size='lg'
-          colorScheme='green'
-          variant='outline'
-          width='100%'
-          maxWidth='30rem'
-          onClick={handleBack}
-        >
-          店舗を再検索する
-        </Button>
-      </Flex>
+        <Flex justifyContent='center'>
+          <Button
+            size='lg'
+            type='submit'
+            colorScheme='green'
+            width='100%'
+            maxWidth='30rem'
+          >
+            検索
+          </Button>
+        </Flex>
+        <Flex justifyContent='center' mt={8}>
+          <Button
+            size='lg'
+            colorScheme='green'
+            variant='outline'
+            width='100%'
+            maxWidth='30rem'
+            onClick={handleBack}
+          >
+            店舗を再検索する
+          </Button>
+        </Flex>
+      </form>
     </div>
   );
 });
