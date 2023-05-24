e109f478901645abddc1bc7f8afcbe9a170dd20c
centerRyo <abc.atrn@gmail.com>
2023-05-24 17:51:57 +0900

    クエリパラメータ~を設定できるようにする

--- frontend/src/pages/menus/index.tsx
+++ frontend/src/pages/menus/index.tsx
@@ -1,6 +1,11 @@
 import { Menus } from '@/features/Menus';
 import { NextPage } from 'next';
 
+export type OptionalQuery = {
+  shop_id: number;
+  excluded_ingredient_ids: string;
+};
+
 const MenusPage: NextPage = () => {
   return <Menus />;
 };
